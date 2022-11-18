8cfcf4c080f7a814aabdf041cad7c0a5bd42cb50
Thijmen Heuvelink <mail@thijmenheuvelink.nl>
2022-11-18 22:28:58 +0100

    embed data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,12 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Theme Builder - Directus</title>
         <link rel="icon" href="/favicon.ico" />
+
+        <meta content="Theme Builder" property="og:title" />
+        <meta content="A Theme Builder for Directus" property="og:description" />
+        <meta content="https://themebuilder.thijmenheuvelink.nl" property="og:url" />
+        <meta content="https://i.imgur.com/xUqSnkA.png" property="og:image" />
+        <meta content="#6644FF" data-react-helmet="true" name="theme-color" />
       </Head>
 
       <Component {...pageProps} />
