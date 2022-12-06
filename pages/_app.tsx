3395ec9809404728e61d36f1421be5bf7b46c895
Thijmen Heuvelink <mail@thijmenheuvelink.nl>
2022-12-06 08:53:06 +0100

    Prune

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,10 @@
 import Head from 'next/head'
-import { Directus } from '@directus/sdk'
 
 import type { AppProps } from 'next/app'
 
 import '/source/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-
-  pageProps.API = new Directus(process.env.DIRECTUS_ENDPOINT || "http://127.0.0.1:8055")
 
   return (
     <>
