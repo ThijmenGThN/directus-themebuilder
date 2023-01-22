78d09ce59920573a3c3eaa74fcfa4313a5fa379a
Thijmen Heuvelink <mail@thijmenheuvelink.nl>
2023-01-22 23:56:06 +0100

    🔥 Removed endpoint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,8 @@
 import Head from 'next/head'
-import getConfig from 'next/config'
-import { Directus } from '@directus/sdk'
 
 import '@/styles/globals.css'
 
-const { publicRuntimeConfig: config } = getConfig()
-
 export default function App({ Component, pageProps }: any) {
-
-  pageProps.API = new Directus(config.CORS_ENDPOINT ? config.CORS_ENDPOINT : 'http://localhost:8055')
 
   return (
     <>
