f7c9b30f04a126cca582cbffe9a5a124d21e92ae
Thijmen Heuvelink <mail@thijmenheuvelink.nl>
2023-01-24 20:18:30 +0000

    ✨ Credits and Relocated Function

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,34 +28,14 @@ export default function Index({ git }: any) {
     out: (obj: any) => obj.style.borderColor = '#d4d4d4'
   }
 
-
-  /*
-    Never uses next before... There is proberly a bettter place for this function.
-  */
-  function pickTextColorBasedOnBgColorAdvanced(bgColor: any, lightColor: any, darkColor: any) {
-    var color = (bgColor.charAt(0) === '#') ? bgColor.substring(1, 7) : bgColor;
-    var r = parseInt(color.substring(0, 2), 16); // hexToR
-    var g = parseInt(color.substring(2, 4), 16); // hexToG
-    var b = parseInt(color.substring(4, 6), 16); // hexToB
-    var uicolors = [r / 255, g / 255, b / 255];
-    var c = uicolors.map((col) => {
-      if (col <= 0.03928) {
-        return col / 12.92;
-      }
-      return Math.pow((col + 0.055) / 1.055, 2.4);
-    });
-    var L = (0.2126 * c[0]) + (0.7152 * c[1]) + (0.0722 * c[2]);
-    return (L > 0.179) ? darkColor : lightColor;
-  }
-
-
-
   useEffect(() => {
     // Ensure hex starts with a "#".
     !hex.startsWith('#') && setHex('#' + hex)
-    // Set hex text to black to fix white text on light backgrounds
-    //todo: instead of black, choose one of the darker shadewrap colors
-    setHexText(pickTextColorBasedOnBgColorAdvanced(hex,hex,"#000000"))
+
+    // Ensures that any given hex color can be seen infront of a white background.
+    // -->> Implementation by https://github.com/vanling - Thanks!
+    setHexText(shade.contrast(hex, hex, '#000'))
+
     // Update the css object.
     setContent(shade.wrap(hex))
   }, [hex])
