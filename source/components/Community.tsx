3b1e56fbe5c48d0dccf494ed2f1b5109dd173dd7
Thijmen Heuvelink <mail@thijmenheuvelink.nl>
2023-10-17 23:00:49 +0200

    📝 Reworded a single line.

--- source/components/Community.tsx
+++ source/components/Community.tsx
@@ -30,7 +30,7 @@ export default function Community() {
                                 <div className="mx-auto max-w-2xl lg:mx-0">
                                     <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contributors</h2>
                                     <p className="mt-6 text-lg leading-8 text-gray-600">
-                                        The wizards who built and maintain this project - Join us on an adventure!
+                                        The peeps who built and maintain this project.
                                     </p>
                                 </div>
 
